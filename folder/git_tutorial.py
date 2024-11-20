9d6101899d2209caef573ea0d865818df4fe54db
Chris Knoblauch <cjknobla@gmail.com>
2024-11-20 14:55:21 -0500

    summarized git workflow in readme

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -2,5 +2,10 @@ def add_numbers(a, b, c):
     """Return the sum of a, b, and c"""
     return sum([a, b, c])
 
+#1. Extend add_numbers to allow an arbitrary number of parameters
+#2. Investigate and resolve security exploit of add_numbers()
+#3. Make the implementation of add_numbers() more performant
+#4. Add documentation for add_numbers()
+
 def calc_difference(a, b):
     return a - b
